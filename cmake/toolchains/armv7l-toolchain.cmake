33495e8904521bc05aa965b6676e5ee2b65f208e
Memet Bilgin <memetb@gmail.com>
2025-07-04 12:40:13 -0300

    reorder linker flag declarations to prevent overwriting

--- cmake/toolchains/armv7l-toolchain.cmake
+++ cmake/toolchains/armv7l-toolchain.cmake
@@ -13,11 +13,12 @@ set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Os")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffunction-sections -fdata-sections")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ffunction-sections -fdata-sections")
 
 set(CMAKE_EXE_LINKER_FLAGS "-static")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
 
 set(ENV{PKG_CONFIG_PATH} "")
 set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
