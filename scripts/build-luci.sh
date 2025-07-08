3bec5d78c042304eb39b0188b6ac3c28ede69c5f
Memet Bilgin <memetb@gmail.com>
2025-07-08 13:05:46 -0300

    add luci interface

---
+++ scripts/build-luci.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# THIS SCRIPT IS A QUICK AND DIRTY PLACEHOLDER BEFORE PROPERY OPENWRT INTEGRATION
+
+name=luci-zmq-proxy
+version="1.0.0"
+
+temp_dir=$(mktemp -d)
+source_dir=$(realpath ..)
+
+mkdir -p ../ipk-output
+
+mkdir -p $temp_dir/data/
+mkdir -p $temp_dir/control
+touch $temp_dir/control/{control,postinst,preinst,prerm}
+chmod 644 $temp_dir/control/control
+chmod 755 $temp_dir/control/{postinst,prerm,preinst}
+
+cp -R ../package/$name/files/* $temp_dir/data
+
+cat <<EOF > $temp_dir/control/control
+Package: $name
+Version: $version
+Architecture: noarch
+Maintainer: memetb@gmail.com
+Description: ZMQ proxy server user interface
+Priority: optional
+Section: utils
+Depends: zmq-proxy luci
+EOF
+
+cat $temp_dir/control/control
+
+cat <<EOF $temp_dir/control/preinst
+#!/bin/sh
+EOF
+
+cat <<EOF > $temp_dir/control/postinst
+#!/bin/sh
+EOF
+
+cat <<EOF > $temp_dir/control/prerm
+#!/bin/sh
+EOF
+
+echo "2.0" > $temp_dir/debian-binary
+
+pushd $temp_dir
+tree
+
+cd control
+tar --numeric-owner --group=0 --owner=0 -zcvf ../control.tar.gz ./*
+cd ..
+
+cd data
+tar --numeric-owner --group=0 --owner=0 -zcvf ../data.tar.gz ./*
+cd ..
+
+rm -f $source_dir/ipk-output/$name_$version-noarch.ipk
+tar --numeric-owner --group=0 --owner=0 -zcf \
+    $source_dir/ipk-output/${name}_${version}-noarch.ipk \
+    ./debian-binary ./data.tar.gz ./control.tar.gz
